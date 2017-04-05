7daa2a874f7bd3640ea5c4b085623472009482e2
Ilia Bozhinov <loopme99@hotmail.com>
2017-04-05 10:04:54 +0300

    Add snap_signal.hpp

---
+++ plugins/single_plugins/snap_signal.hpp
@@ -0,0 +1,23 @@
+#ifndef SNAP_SIGNAL
+#define SNAP_SIGNAL
+
+#include <output.hpp>
+
+enum slot_type {
+    SLOT_BL     = 1,
+    SLOT_BOTTOM = 2,
+    SLOT_BR     = 3,
+    SLOT_LEFT   = 4,
+    SLOT_CENTER = 5,
+    SLOT_RIGHT  = 6,
+    SLOT_TL     = 7,
+    SLOT_TOP    = 8,
+    SLOT_TR     = 9,
+};
+
+struct snap_signal : public signal_data {
+    wayfire_view view;
+    slot_type tslot;
+};
+
+#endif /* end of include guard: SNAP_SIGNAL */
