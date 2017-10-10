bf400a908486d6eaa566d055688a85e3dfd32f50
Ilia Bozhinov <iliyabo@hotmail.com>
2017-10-10 19:44:40 +0300

    gles-renderer: clean up fragment shader

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -2,19 +2,10 @@
 
 varying highp vec2 uvpos;
 
-
 uniform sampler2D smp;
 uniform mediump vec4      color;
 
-//out mediump vec4 gl_FragColor;
-
-
-void main() {
-    //if(depth == 32)
-        gl_FragColor = texture2D(smp, uvpos) * color;
-    //else
-    //gl_FragColor = vec4(1, 0, 0, 1);
-//        gl_FragColor = vec4(texture2D(smp, uvpos).xyz, 1);
-    //if(bgra == 1)
-    //    gl_FragData[0] = gl_FragData[0].zyxw;
+void main()
+{
+    gl_FragColor = texture2D(smp, uvpos) * color;
 }
