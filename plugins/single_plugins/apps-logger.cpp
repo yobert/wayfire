ff4ada9274ff31d224aff092b823df08382b4131
Ilia Bozhinov <ammen99@gmail.com>
2018-05-24 08:44:35 +0300

    reimplement apps-logger plugin

--- plugins/single_plugins/apps-logger.cpp
+++ plugins/single_plugins/apps-logger.cpp
@@ -20,21 +20,16 @@ class wayfire_apps_logger : public wayfire_plugin_t
 
     void update_log_file()
     {
-        std::ofstream out("/tmp/wayfire-app-list",
+        std::ofstream out("/tmp/wayfire-app-list-" + std::to_string(output->id),
                           std::ofstream::trunc | std::ofstream::out);
 
         size_t id = 1;
         output->workspace->for_each_view([&out, &id] (wayfire_view view)
         {
-            auto c_app_id = weston_desktop_surface_get_app_id(view->desktop_surface);
-            std::string app_id = c_app_id ? c_app_id : "(null)";
-
-            auto c_title = weston_desktop_surface_get_title (view->desktop_surface);
-            std::string title = c_title ? c_title : "(null)";
-
-            out << "no. " << id << " app_id: " << app_id << " title: " << title << std::endl;
+            if (view->is_mapped())
+            out << "no. " << id << " app_id: " << view->get_app_id() << " title: " << view->get_title() << std::endl;
             ++id;
-        });
+        }, WF_LAYER_WORKSPACE);
     }
 
     function idle_func = [=] () { update_log_file(); };
@@ -44,17 +39,15 @@ class wayfire_apps_logger : public wayfire_plugin_t
     {
         created_cb = [=] (signal_data *data)
         {
-            wl_event_loop_add_idle(wl_display_get_event_loop(core->ec->wl_display),
-                                   idle_callback, &idle_func);
+            wl_event_loop_add_idle(core->ev_loop, idle_callback, &idle_func);
         };
-        output->connect_signal("create-view", &created_cb);
+        output->connect_signal("map-view", &created_cb);
 
         destroyed_cb = [=] (signal_data *data)
         {
-            wl_event_loop_add_idle(wl_display_get_event_loop(core->ec->wl_display),
-                                   idle_callback, &idle_func);
+            wl_event_loop_add_idle(core->ev_loop, idle_callback, &idle_func);
         };
-        output->connect_signal("destroy-view", &destroyed_cb);
+        output->connect_signal("unmap-view", &destroyed_cb);
     }
 };
 
