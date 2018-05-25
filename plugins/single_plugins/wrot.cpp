c415a43d40f158d9eb9732fc4d1c1a74433bd7c9
Ilia Bozhinov <ammen99@gmail.com>
2018-05-25 08:05:03 +0300

    wrot: don't rotate constantly, but following the mouse

--- plugins/single_plugins/wrot.cpp
+++ plugins/single_plugins/wrot.cpp
@@ -3,21 +3,36 @@
 #include "output.hpp"
 #include <linux/input.h>
 
+static double cross (double x1, double y1, double x2, double y2) // cross product
+{
+    return x1 * y2 - x2 * y1;
+}
+
+static double vlen(double x1, double y1) // length of vector centered at the origin
+{
+    return std::sqrt(x1 * x1 + y1 * y1);
+}
+
 class wf_wrot : public wayfire_plugin_t
 {
     button_callback call;
+
+    int last_x, last_y;
     public:
         void init(wayfire_config *config)
         {
-            call = [=] (uint32_t x, int, int)
+            call = [=] (uint32_t, int x, int y)
             {
                 output->activate_plugin(grab_interface);
                 grab_interface->grab();
+
+                last_x = x;
+                last_y = y;
             };
 
             output->add_button(WLR_MODIFIER_ALT, BTN_RIGHT, &call);
 
-            grab_interface->callbacks.pointer.motion = [=] (int, int)
+            grab_interface->callbacks.pointer.motion = [=] (int x, int y)
             {
                 auto view = output->get_top_view();
 
@@ -25,9 +40,25 @@ class wf_wrot : public wayfire_plugin_t
                     view->set_transformer(std::unique_ptr<wf_2D_view> (new wf_2D_view(output)));
 
                 auto tr = dynamic_cast<wf_2D_view*> (view->get_transformer());
+
                 view->damage();
-                tr->angle += 0.02;
+
+
+                auto g = view->get_wm_geometry();
+
+                double cx = g.x + g.width / 2.0;
+                double cy = g.y + g.height / 2.0;
+
+                double x1 = last_x - cx, y1 = last_y - cy;
+                double x2 = x - cx, y2 = y - cy;
+
+                /* cross(a, b) = |a| * |b| * sin(a, b) */
+                tr->angle += std::asin(cross(x1, y1, x2, y2) / vlen(x1, y1) / vlen(x2, y2));
+
                 view->damage();
+
+                last_x = x;
+                last_y = y;
             };
 
             grab_interface->callbacks.pointer.button = [=] (uint32_t, uint32_t s)
