dd97f03db4bb89a65829f07d162742064ea45ef4
Ilia Bozhinov <ammen99@gmail.com>
2018-07-25 15:34:27 +0300

    simple-decorations: render initial title

--- plugins/decor/deco-subsurface.cpp
+++ plugins/decor/deco-subsurface.cpp
@@ -6,26 +6,66 @@ extern "C"
 #undef static
 }
 
+#define GLM_FORCE_RADIANS
+#include <glm/gtc/matrix_transform.hpp>
+
 #include <linux/input-event-codes.h>
 
 #include <compositor-surface.hpp>
 #include <output.hpp>
+#include <opengl.hpp>
 #include <core.hpp>
 #include <debug.hpp>
 #include <decorator.hpp>
+#include <view-transform.hpp>
 #include <signal-definitions.hpp>
 #include "deco-subsurface.hpp"
 
 #include <cairo.h>
 
-GLuint get_text_texture(std::string text)
+GLuint get_text_texture(int width, int height, std::string text)
 {
+    const auto format = CAIRO_FORMAT_ARGB32;
+    auto surface = cairo_image_surface_create(format, width, height);
+    auto cr = cairo_create(surface);
+
+    const float font_scale = 0.8;
+    const float font_size = height * font_scale;
+
+    // render text
+    cairo_select_font_face(cr, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_source_rgba(cr, 1, 1, 1, 1);
+
+    cairo_set_font_size(cr, font_size);
+
+    cairo_text_extents_t ext;
+    cairo_text_extents(cr, text.c_str(), &ext);
+
+    cairo_move_to(cr, font_size, height - ext.height / 2.0);
+    cairo_show_text(cr, text.c_str());
+
+    cairo_destroy(cr);
+
+    auto src = cairo_image_surface_get_data(surface);
+
+    GLuint tex;
+    GL_CALL(glGenTextures(1, &tex));
+    GL_CALL(glBindTexture(GL_TEXTURE_2D, tex));
+    GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, src));
+
+    cairo_surface_destroy(surface);
+
+    return tex;
 }
 
 class simple_decoration_surface : public wayfire_compositor_surface_t, public wf_decorator_frame_t
 {
+    const int titlebar_thickness = 30;
     const int normal_thickness = 15;
+
     int thickness = normal_thickness;
+    int titlebar = titlebar_thickness;
+
     wayfire_view view;
 
     protected:
@@ -38,7 +78,7 @@ class simple_decoration_surface : public wayfire_compositor_surface_t, public wf
         {
             view->get_child_offset(x, y);
             x -= thickness;
-            y -= thickness;
+            y -= titlebar;
         }
 
     public:
@@ -56,6 +96,7 @@ class simple_decoration_surface : public wayfire_compositor_surface_t, public wf
             return true;
         }
 
+        const int text_field_width = 500;
         int width = 100, height = 100;
         virtual wf_geometry get_output_geometry()
         {
@@ -66,6 +107,9 @@ class simple_decoration_surface : public wayfire_compositor_surface_t, public wf
         bool active = true; // when views are mapped, they are usually activated
         float border_color[4] = {0.15f, 0.15f, 0.15f, 0.8f};
         float border_color_inactive[4] = {0.25f, 0.25f, 0.25f, 0.95f};
+
+        GLuint tex = -1;
+
 
         virtual void _wlr_render_box(const wlr_fb_attribs& fb, int x, int y, const wlr_box& scissor)
         {
@@ -82,6 +126,23 @@ class simple_decoration_surface : public wayfire_compositor_surface_t, public wf
             auto sbox = scissor; wlr_renderer_scissor(core->renderer, &sbox);
 
             wlr_render_quad_with_matrix(core->renderer, active ? border_color : border_color_inactive, matrix);
+
+            if (tex == (uint)-1)
+                tex = get_text_texture(text_field_width, titlebar, view->get_title());
+
+            auto ortho = glm::ortho(0.0f, 1.0f * fb.width, 1.0f * fb.height, 0.0f);
+
+            gl_geometry gg;
+            gg.x1 = geometry.x;
+            gg.y1 = geometry.y;
+            gg.x2 = geometry.x + std::min(text_field_width, geometry.width);
+            gg.y2 = geometry.y + titlebar;
+
+            log_info("render tex %fx%f %fx%f", gg.x1, gg.y1, gg.x2, gg.y2);
+
+            OpenGL::use_default_program();
+            OpenGL::render_transformed_texture(tex, gg, {}, ortho, {1, 1, 1, 1}, TEXTURE_TRANSFORM_INVERT_Y);
+
             wlr_renderer_end(core->renderer);
         }
 
@@ -93,7 +154,7 @@ class simple_decoration_surface : public wayfire_compositor_surface_t, public wf
             pixman_region32_init(&frame_region);
 
             /* top */
-            pixman_region32_union_rect(&frame_region, &frame_region, x * scale, y * scale, width * scale,     thickness * scale);
+            pixman_region32_union_rect(&frame_region, &frame_region, x * scale, y * scale, width * scale, titlebar * scale);
             /* left */
             pixman_region32_union_rect(&frame_region, &frame_region, x * scale, y * scale, thickness * scale, height * scale);
             /* right */
@@ -159,9 +220,9 @@ class simple_decoration_surface : public wayfire_compositor_surface_t, public wf
         virtual wf_geometry expand_wm_geometry(wf_geometry contained_wm_geometry)
         {
             contained_wm_geometry.x -= thickness;
-            contained_wm_geometry.y -= thickness;
+            contained_wm_geometry.y -= titlebar;
             contained_wm_geometry.width += 2 * thickness;
-            contained_wm_geometry.height += 2 * thickness;
+            contained_wm_geometry.height += thickness + titlebar;
 
             return contained_wm_geometry;
         }
@@ -169,7 +230,7 @@ class simple_decoration_surface : public wayfire_compositor_surface_t, public wf
         virtual void calculate_resize_size(int& target_width, int& target_height)
         {
             target_width -= 2 * thickness;
-            target_height -= 2 * thickness;
+            target_height -= thickness + titlebar;
         }
 
         virtual void notify_view_activated(bool active)
