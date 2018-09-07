e56acb600d996ed64d1e91073451a0a910f16d18
Ilia Bozhinov <ammen99@gmail.com>
2018-09-07 17:51:54 +0300

    input: react to keyboard option changes in config file
    partial work towards #63

--- src/core/seat/keyboard.cpp
+++ src/core/seat/keyboard.cpp
@@ -14,17 +14,17 @@ extern "C"
 
 void handle_keyboard_destroy_cb(wl_listener *listener, void*)
 {
-    wf_keyboard *keyboard = wl_container_of(listener, keyboard, destroy);
-    core->input->handle_input_destroyed(keyboard->device);
+    wf_keyboard::listeners *lss = wl_container_of(listener, lss, destroy);
+    core->input->handle_input_destroyed(lss->keyboard->device);
 }
 
 static void handle_keyboard_key_cb(wl_listener* listener, void *data)
 {
     auto ev = static_cast<wlr_event_keyboard_key*> (data);
-    wf_keyboard *keyboard = wl_container_of(listener, keyboard, key);
+    wf_keyboard::listeners *lss = wl_container_of(listener, lss, key);
 
     auto seat = core->get_current_seat();
-    wlr_seat_set_keyboard(seat, keyboard->device);
+    wlr_seat_set_keyboard(seat, lss->keyboard->device);
 
     if (!core->input->handle_keyboard_key(ev->keycode, ev->state))
     {
@@ -38,10 +38,10 @@ static void handle_keyboard_key_cb(wl_listener* listener, void *data)
 static void handle_keyboard_mod_cb(wl_listener* listener, void* data)
 {
     auto kbd = static_cast<wlr_keyboard*> (data);
-    wf_keyboard *keyboard = wl_container_of(listener, keyboard, modifier);
+    wf_keyboard::listeners *lss = wl_container_of(listener, lss, modifier);
 
     auto seat = core->get_current_seat();
-    wlr_seat_set_keyboard(seat, keyboard->device);
+    wlr_seat_set_keyboard(seat, lss->keyboard->device);
     wlr_seat_keyboard_send_modifiers(core->input->seat, &kbd->modifiers);
 
     wlr_idle_notify_activity(core->protocols.idle, seat);
@@ -50,42 +50,79 @@ static void handle_keyboard_mod_cb(wl_listener* listener, void* data)
 wf_keyboard::wf_keyboard(wlr_input_device *dev, wayfire_config *config)
     : handle(dev->keyboard), device(dev)
 {
-    auto ctx = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
-
     auto section = config->get_section("input");
 
-    std::string model   = *section->get_option("xkb_model", "");
-    std::string variant = *section->get_option("xkb_variant", "");
-    std::string layout  = *section->get_option("xkb_layout", "");
-    std::string options = *section->get_option("xkb_option", "");
-    std::string rules   = *section->get_option("xkb_rule", "");
+    model   = section->get_option("xkb_model", "");
+    variant = section->get_option("xkb_variant", "");
+    layout  = section->get_option("xkb_layout", "");
+    options = section->get_option("xkb_option", "");
+    rules   = section->get_option("xkb_rule", "");
+
+    repeat_rate  = section->get_option("kb_repeat_rate", "40");
+    repeat_delay = section->get_option("kb_repeat_delay", "400");
+
+    keyboard_options_updated = [=] () {
+        reload_input_options();
+    };
+
+    model->add_updated_handler(&keyboard_options_updated);
+    rules->add_updated_handler(&keyboard_options_updated);
+    layout->add_updated_handler(&keyboard_options_updated);
+    variant->add_updated_handler(&keyboard_options_updated);
+    options->add_updated_handler(&keyboard_options_updated);
+    repeat_rate->add_updated_handler(&keyboard_options_updated);
+    repeat_delay->add_updated_handler(&keyboard_options_updated);
+
+    lss.keyboard = this;
+    lss.key.notify      = handle_keyboard_key_cb;
+    lss.modifier.notify = handle_keyboard_mod_cb;
+    lss.destroy.notify  = handle_keyboard_destroy_cb;
+
+    wl_signal_add(&dev->keyboard->events.key,       &lss.key);
+    wl_signal_add(&dev->keyboard->events.modifiers, &lss.modifier);
+    wl_signal_add(&dev->events.destroy,             &lss.destroy);
+
+    reload_input_options();
+    wlr_seat_set_keyboard(core->get_current_seat(), dev);
+}
+
+void wf_keyboard::reload_input_options()
+{
+    auto ctx = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+
+    auto rules = this->rules->as_string();
+    auto model = this->model->as_string();
+    auto layout = this->layout->as_string();
+    auto variant = this->variant->as_string();
+    auto options = this->options->as_string();
 
     xkb_rule_names names;
-    names.rules   = strdup(rules.c_str());
-    names.model   = strdup(model.c_str());
-    names.layout  = strdup(layout.c_str());
-    names.variant = strdup(variant.c_str());
-    names.options = strdup(options.c_str());
+    names.rules   = rules.c_str();
+    names.model   = model.c_str();
+    names.layout  = layout.c_str();
+    names.variant = variant.c_str();
+    names.options = options.c_str();
 
     auto keymap = xkb_map_new_from_names(ctx, &names, XKB_KEYMAP_COMPILE_NO_FLAGS);
 
-    wlr_keyboard_set_keymap(dev->keyboard, keymap);
+    wlr_keyboard_set_keymap(handle, keymap);
+
     xkb_keymap_unref(keymap);
     xkb_context_unref(ctx);
 
-    int repeat_rate  = *section->get_option("kb_repeat_rate", "40");
-    int repeat_delay = *section->get_option("kb_repeat_delay", "400");
-    wlr_keyboard_set_repeat_info(dev->keyboard, repeat_rate, repeat_delay);
+    wlr_keyboard_set_repeat_info(handle, repeat_rate->as_int(),
+                                 repeat_delay->as_int());
+}
 
-    key.notify      = handle_keyboard_key_cb;
-    modifier.notify = handle_keyboard_mod_cb;
-    destroy.notify  = handle_keyboard_destroy_cb;
-
-    wl_signal_add(&dev->keyboard->events.key, &key);
-    wl_signal_add(&dev->keyboard->events.modifiers, &modifier);
-    wl_signal_add(&dev->events.destroy, &destroy);
-
-    wlr_seat_set_keyboard(core->get_current_seat(), dev);
+wf_keyboard::~wf_keyboard()
+{
+    model->rem_updated_handler(&keyboard_options_updated);
+    rules->rem_updated_handler(&keyboard_options_updated);
+    layout->rem_updated_handler(&keyboard_options_updated);
+    variant->rem_updated_handler(&keyboard_options_updated);
+    options->rem_updated_handler(&keyboard_options_updated);
+    repeat_rate->rem_updated_handler(&keyboard_options_updated);
+    repeat_delay->rem_updated_handler(&keyboard_options_updated);
 }
 
 /* input manager things */
--- src/core/seat/keyboard.hpp
+++ src/core/seat/keyboard.hpp
@@ -14,9 +14,21 @@ struct wf_keyboard
 {
     wlr_keyboard *handle;
     wlr_input_device *device;
-    wl_listener key, modifier, destroy;
+
+    /* so that we can safely use wl_container_of */
+    struct listeners
+    {
+        wl_listener key, modifier, destroy;
+        wf_keyboard *keyboard;
+    } lss;
+
+    wf_option model, variant, layout, options, rules;
+    wf_option repeat_rate, repeat_delay;
+    wf_option_callback keyboard_options_updated;
 
     wf_keyboard(wlr_input_device *keyboard, wayfire_config *config);
+    void reload_input_options();
+    ~wf_keyboard();
 };
 
 struct key_callback_data : wf_callback
