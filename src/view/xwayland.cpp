yobert/wayfire

commit 1523baf3173fb475c68920f33cdaf9b689b65675

core: implement support for xdg-decoration

src/view/xwayland.cpp
@@ -217,7 +217,8 @@ class wayfire_unmanaged_xwayland_view : public wayfire_xwayland_view_base
 class wayfire_xwayland_view : public wayfire_xwayland_view_base
 {
     wf::wl_listener_wrapper on_request_move, on_request_resize,
-        on_request_maximize, on_request_fullscreen, on_set_parent;
+        on_request_maximize, on_request_fullscreen, on_set_parent,
+        on_set_decorations;
 
   public:
     wayfire_xwayland_view(wlr_xwayland_surface *xww)
@@ -250,7 +251,13 @@ class wayfire_xwayland_view : public wayfire_xwayland_view_base
                 set_toplevel_parent(parent);
         });
 
+        on_set_decorations.set_callback([&] (void*) {
+            update_decorated();
+        });
+
         on_set_parent.connect(&xw->events.set_parent);
+        on_set_decorations.connect(&xw->events.set_decorations);
+
         on_request_move.connect(&xw->events.request_move);
         on_request_resize.connect(&xw->events.request_resize);
         on_request_maximize.connect(&xw->events.request_maximize);
@@ -259,6 +266,7 @@ class wayfire_xwayland_view : public wayfire_xwayland_view_base
         xw->data = dynamic_cast<wf::view_interface_t*> (this);
         // set initial parent
         on_set_parent.emit(nullptr);
+        on_set_decorations.emit(nullptr);
     }
 
     virtual void destroy() override
@@ -342,11 +350,11 @@ class wayfire_xwayland_view : public wayfire_xwayland_view_base
         last_server_height = geometry.height;
     }
 
-    virtual bool should_be_decorated() override
+    void update_decorated()
     {
-        return !(xw->decorations &
-            (WLR_XWAYLAND_SURFACE_DECORATIONS_NO_TITLE |
-             WLR_XWAYLAND_SURFACE_DECORATIONS_NO_BORDER));
+        uint32_t csd_flags = WLR_XWAYLAND_SURFACE_DECORATIONS_NO_TITLE |
+                WLR_XWAYLAND_SURFACE_DECORATIONS_NO_BORDER;
+        this->set_decoration_mode(xw->decorations & csd_flags);
     }
 
     void set_activated(bool active) override

plugins/decor/decoration.cpp
@@ -8,23 +8,24 @@
 #include "deco-subsurface.hpp"
 class wayfire_decoration : public wf::plugin_interface_t
 {
-    wf::signal_callback_t view_created;
+    wf::signal_connection_t view_updated {
+        [=] (wf::signal_data_t *data)
+        {
+            update_view_decoration(get_signaled_view(data));
+        }};
 
   public:
     void init() override
     {
         grab_interface->name = "simple-decoration";
         grab_interface->capabilities = wf::CAPABILITY_VIEW_DECORATOR;
-        view_created = [=] (wf::signal_data_t *data)
-        {
-            new_view(get_signaled_view(data));
-        };
 
-        output->connect_signal("map-view", &view_created);
+        output->connect_signal("map-view", &view_updated);
+        output->connect_signal("decoration-state-updated-view", &view_updated);
     }
 
     wf::wl_idle_call idle_deactivate;
-    void new_view(wayfire_view view)
+    void update_view_decoration(wayfire_view view)
     {
         if (view->should_be_decorated())
         {
@@ -35,6 +36,9 @@ class wayfire_decoration : public wf::plugin_interface_t
                     output->deactivate_plugin(grab_interface);
                 });
             }
+        } else
+        {
+            view->set_decoration(nullptr);
         }
     }
 
@@ -42,8 +46,6 @@ class wayfire_decoration : public wf::plugin_interface_t
     {
         for (auto& view : output->workspace->get_views_in_layer(wf::ALL_LAYERS))
             view->set_decoration(nullptr);
-
-        output->disconnect_signal("map-view", &view_created);
     }
 };
 

src/api/wayfire/core.hpp
@@ -22,6 +22,7 @@ extern "C"
     struct wlr_xdg_output_manager_v1;
     struct wlr_export_dmabuf_manager_v1;
     struct wlr_server_decoration_manager;
+    struct wlr_xdg_decoration_manager_v1;
     struct wlr_input_inhibit_manager;
     struct wlr_virtual_keyboard_manager_v1;
     struct wlr_virtual_pointer_manager_v1;
@@ -86,6 +87,7 @@ class compositor_core_t : public wf::object_base_t
         wlr_linux_dmabuf_v1 *linux_dmabuf;
         wlr_export_dmabuf_manager_v1 *export_dmabuf;
         wlr_server_decoration_manager *decorator_manager;
+        wlr_xdg_decoration_manager_v1 *xdg_decorator;
         wlr_xdg_output_manager_v1 *output_manager;
         wlr_virtual_keyboard_manager_v1 *vkbd_manager;
         wlr_virtual_pointer_manager_v1 *vptr_manager;

src/api/wayfire/signal-definitions.hpp
@@ -162,6 +162,14 @@ namespace wf
         /* The event as it has arrived from wlroots */
         wlr_event_t *event;
     };
+
+    /**
+     * decoration-state-updated signal is emitted when the value of
+     * view::should_be_decorated() changes.
+     *
+     * decoration-state-updated-view is emitted on the output of the view.
+     */
+    using decoration_state_updated_signal = _view_signal;
 }
 
 #endif

src/core/core-impl.hpp
@@ -81,6 +81,7 @@ class compositor_core_impl_t : public compositor_core_t
   private:
     wf::wl_listener_wrapper output_layout_changed;
     wf::wl_listener_wrapper decoration_created;
+    wf::wl_listener_wrapper xdg_decoration_created;
     wf::wl_listener_wrapper vkbd_created;
     wf::wl_listener_wrapper vptr_created;
     wf::wl_listener_wrapper input_inhibit_activated;

src/core/core.cpp
@@ -12,6 +12,7 @@ extern "C"
 #include <wlr/types/wlr_linux_dmabuf_v1.h>
 #include <wlr/types/wlr_export_dmabuf_v1.h>
 #include <wlr/types/wlr_server_decoration.h>
+#include <wlr/types/wlr_xdg_decoration_v1.h>
 #include <wlr/types/wlr_gamma_control_v1.h>
 #include <wlr/types/wlr_xdg_output_v1.h>
 #include <wlr/types/wlr_screencopy_v1.h>
@@ -76,6 +77,52 @@ struct wf_server_decoration_t
         on_destroy.connect(&decor->events.destroy);
         /* Read initial decoration settings */
         mode_set(NULL);
+    }
+};
+
+struct wf_xdg_decoration_t
+{
+    wlr_xdg_toplevel_decoration_v1 *decor;
+    wf::wl_listener_wrapper on_mode_request, on_commit, on_destroy;
+
+    std::function<void(void*)> mode_request = [&] (void*)
+    {
+        auto mode = decor->client_pending_mode;
+        LOGE("mode request ", mode);
+        if (mode == WLR_XDG_TOPLEVEL_DECORATION_V1_MODE_NONE)
+            mode = WLR_XDG_TOPLEVEL_DECORATION_V1_MODE_CLIENT_SIDE;
+        wlr_xdg_toplevel_decoration_v1_set_mode(decor, mode);
+    };
+
+    std::function<void(void*)> commit = [&] (void*)
+    {
+        bool use_csd =
+            decor->current_mode == WLR_XDG_TOPLEVEL_DECORATION_V1_MODE_CLIENT_SIDE;
+        LOGE(decor->surface->toplevel->title, ": set use CSD to ", use_csd);
+        wf::get_core_impl().uses_csd[decor->surface->surface] = use_csd;
+
+        auto wf_surface = dynamic_cast<wf::wlr_view_t*> (
+            wf::wf_surface_from_void(decor->surface->data));
+        if (wf_surface)
+            wf_surface->set_decoration_mode(use_csd);
+    };
+
+    wf_xdg_decoration_t(wlr_xdg_toplevel_decoration_v1 *_decor)
+        : decor(_decor)
+    {
+        LOGE("new xdg decoration");
+        on_mode_request.set_callback(mode_request);
+        on_commit.set_callback(commit);
+        on_destroy.set_callback([&] (void *) {
+            wf::get_core_impl().uses_csd.erase(decor->surface->surface);
+            delete this;
+        });
+
+        on_mode_request.connect(&decor->events.request_mode);
+        on_commit.connect(&decor->surface->surface->events.commit);
+        on_destroy.connect(&decor->events.destroy);
+        /* Read initial decoration settings */
+        mode_request(NULL);
     }
 };
 
@@ -146,12 +193,19 @@ void wf::compositor_core_impl_t::init()
     /* decoration_manager setup */
     protocols.decorator_manager = wlr_server_decoration_manager_create(display);
     wlr_server_decoration_manager_set_default_mode(protocols.decorator_manager,
-                                                   WLR_SERVER_DECORATION_MANAGER_MODE_CLIENT);
+        WLR_SERVER_DECORATION_MANAGER_MODE_SERVER);
 
     decoration_created.set_callback([&] (void* data) {
         /* will be freed by the destroy request */
         new wf_server_decoration_t((wlr_server_decoration*)(data));});
     decoration_created.connect(&protocols.decorator_manager->events.new_decoration);
+
+    protocols.xdg_decorator = wlr_xdg_decoration_manager_v1_create(display);
+
+    xdg_decoration_created.set_callback([&] (void* data) {
+        /* will be freed by the destroy request */
+        new wf_xdg_decoration_t((wlr_xdg_toplevel_decoration_v1*)(data));});
+    xdg_decoration_created.connect(&protocols.xdg_decorator->events.new_toplevel_decoration);
 
     protocols.vkbd_manager = wlr_virtual_keyboard_manager_v1_create(display);
     vkbd_created.set_callback([&] (void *data) {

src/view/view-impl.cpp
@@ -216,6 +216,21 @@ bool wf::wlr_view_t::should_be_decorated()
     return role == wf::VIEW_ROLE_TOPLEVEL && !has_client_decoration;
 }
 
+void wf::wlr_view_t::set_decoration_mode(bool use_csd)
+{
+    bool was_decorated = should_be_decorated();
+    this->has_client_decoration = use_csd;
+    if ((was_decorated != should_be_decorated()) && is_mapped())
+    {
+        wf::decoration_state_updated_signal data;
+        data.view = self();
+
+        this->emit_signal("decoration-state-updated", &data);
+        if (get_output())
+            get_output()->emit_signal("decoration-state-updated-view", &data);
+    }
+}
+
 void wf::wlr_view_t::set_output(wf::output_t *wo)
 {
     auto old_output = get_output();

src/view/view-impl.hpp
@@ -107,8 +107,8 @@ class wlr_view_t :
     virtual wlr_surface *get_keyboard_focus_surface() override;
 
     virtual bool should_be_decorated() override;
+    virtual void set_decoration_mode(bool use_csd);
     virtual void set_output(wf::output_t*) override;
-
     bool has_client_decoration = true;
 
   protected:

commit 1b87bff15f68265a779511958e05153cb5de4a07

xwayland: disconnect set_decoration handler when destroying
Otherwise, we will disconnect after the memory has been freed.

This is a regression since 1523baf3

src/view/xwayland.cpp
@@ -272,6 +272,7 @@ class wayfire_xwayland_view : public wayfire_xwayland_view_base
     virtual void destroy() override
     {
         on_set_parent.disconnect();
+        on_set_decorations.disconnect();
         on_request_move.disconnect();
         on_request_resize.disconnect();
         on_request_maximize.disconnect();