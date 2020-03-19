4b9a8eb8340b3975150d34917d468250d7111852
Ilia Bozhinov <ammen99@gmail.com>
2020-03-19 15:29:36 +0100

    autostart: add option to automatically launch wf-shell and wf-background
    This helps when starting wayfire without a configuration file.

--- plugins/single_plugins/autostart.cpp
+++ plugins/single_plugins/autostart.cpp
@@ -1,16 +1,37 @@
 #include <wayfire/singleton-plugin.hpp>
 #include <wayfire/core.hpp>
 #include <wayfire/util/log.hpp>
+#include <wayfire/option-wrapper.hpp>
+#include <config.h>
 
 class wayfire_autostart
 {
+    wf::option_wrapper_t<bool> autostart_wf_shell{"autostart/autostart_wf_shell"};
+
   public:
     wayfire_autostart()
     {
         /* Run only once, at startup */
         auto section = wf::get_core().config.get_section("autostart");
+
+        bool panel_manually_started = false;
+        bool background_manually_started = false;
+
         for (const auto& command : section->get_registered_options())
-            wf::get_core().run(command->get_value_str().c_str());
+        {
+            auto cmd = command->get_value_str();
+            wf::get_core().run(cmd);
+
+            if (cmd.find("wf-panel") != std::string::npos)
+                panel_manually_started = true;
+            if (cmd.find("wf-background") != std::string::npos)
+                background_manually_started = true;
+        }
+
+        if (autostart_wf_shell && !panel_manually_started)
+            wf::get_core().run(INSTALL_PREFIX "/bin/wf-panel");
+        if (autostart_wf_shell && !background_manually_started)
+            wf::get_core().run(INSTALL_PREFIX "/bin/wf-background");
     }
 };
 
