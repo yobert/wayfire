06b22f000b24b62791a0684fcb315fb7fa157b89
Ilia Bozhinov <ammen99@gmail.com>
2020-08-31 11:41:31 +0200

    layer-shell: memset prev_state in the constructor
    Prevents UB by reading from uninitialized memory later.

--- src/view/layer-shell.cpp
+++ src/view/layer-shell.cpp
@@ -332,6 +332,7 @@ wayfire_layer_shell_view::wayfire_layer_shell_view(wlr_layer_surface_v1 *lsurf) 
         " layer ", lsurf->current.layer);
 
     role = wf::VIEW_ROLE_DESKTOP_ENVIRONMENT;
+    std::memset(&this->prev_state, 0, sizeof(prev_state));
 
     /* If we already have an output set, then assign it before core assigns us
      * an output */
