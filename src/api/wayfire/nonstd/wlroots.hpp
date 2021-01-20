9776aaaee859bc624d55c690b5e5ea70672df92f
Ilia Bozhinov <ammen99@gmail.com>
2021-01-20 22:12:20 +0100

    Revert "core: add xdg-foreign support (#892)"
    This reverts commit 6f5bb1481977f81e873fe3eed05a582fff2c8b23.

--- src/api/wayfire/nonstd/wlroots.hpp
+++ src/api/wayfire/nonstd/wlroots.hpp
@@ -35,10 +35,6 @@ extern "C"
     struct wlr_gtk_primary_selection_device_manager;
     struct wlr_primary_selection_v1_device_manager;
 
-    struct wlr_xdg_foreign_v1;
-    struct wlr_xdg_foreign_v2;
-    struct wlr_xdg_foreign_registry;
-
     struct wlr_event_pointer_axis;
     struct wlr_event_pointer_motion;
     struct wlr_output_layout;
