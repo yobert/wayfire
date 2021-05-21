0b899d3542d9308f86c3a60715a6cb3201d309e1
Ilia Bozhinov <ammen99@gmail.com>
2021-05-21 10:49:28 +0200

    handle create/remove workspaces

--- plugins/single_plugins/wlr-workspaces.cpp
+++ plugins/single_plugins/wlr-workspaces.cpp
@@ -6,7 +6,7 @@
 
 extern "C"
 {
-#include <wlr/types/wlr_workspace_v1.h>
+#include <wlr/types/wlr_ext_workspace_v1.h>
 }
 
 namespace wf
@@ -14,60 +14,79 @@ namespace wf
 /**
  * The workspaces manager is stored in core, to allow for a single instance.
  */
-class wlr_workspaces_manager : public custom_data_t
+class wlr_ext_workspaces_manager : public custom_data_t
 {
   public:
     int refcount = 0;
-    wlr_workspace_manager_v1 *manager;
-    wlr_workspaces_manager()
+    wlr_ext_workspace_manager_v1 *manager;
+    wlr_ext_workspaces_manager()
     {
-        manager = wlr_workspace_manager_v1_create(wf::get_core().display);
+        manager = wlr_ext_workspace_manager_v1_create(wf::get_core().display);
     }
 };
 
-class wlr_workspaces_intergration : public plugin_interface_t
+class wlr_ext_workspaces_intergration : public plugin_interface_t
 {
   public:
-    wlr_workspace_group_handle_v1 *group;
-    std::vector<std::vector<wlr_workspace_handle_v1*>> workspaces;
+    wlr_ext_workspace_group_handle_v1 *group;
+    std::vector<std::vector<wlr_ext_workspace_handle_v1*>> workspaces;
+    std::vector<std::vector<wl_listener_wrapper>> on_ws_remove;
 
     wf::wl_listener_wrapper on_commit;
+    wf::wl_listener_wrapper on_ws_create;
 
     void init() override
     {
         /* Take ref to manager */
-        auto manager = wf::get_core().get_data_safe<wlr_workspaces_manager>();
+        auto manager = wf::get_core().get_data_safe<wlr_ext_workspaces_manager>();
         ++manager->refcount;
 
         /* Create group & workspaces */
-        group = wlr_workspace_group_handle_v1_create(manager->manager);
+        group = wlr_ext_workspace_group_handle_v1_create(manager->manager);
+        on_ws_create.set_callback([&] (void *data)
+        {
+            auto ev = static_cast<
+                wlr_ext_workspace_group_handle_v1_create_workspace_event*>(data);
+            LOGD("Application requested creation of workspace ", ev->name);
+        });
+        on_ws_create.connect(&group->events.create_workspace_request);
 
         dimensions_t ws_dim = output->workspace->get_workspace_grid_size();
         workspaces.resize(ws_dim.height,
-            std::vector<wlr_workspace_handle_v1*>(ws_dim.width));
+            std::vector<wlr_ext_workspace_handle_v1*>(ws_dim.width));
+        on_ws_remove.resize(ws_dim.height,
+            std::vector<wl_listener_wrapper>(ws_dim.width));
         for (int i = 0; i < ws_dim.height; i++)
         {
             for (int j = 0; j < ws_dim.width; j++)
             {
-                workspaces[i][j] = wlr_workspace_handle_v1_create(group);
+                workspaces[i][j] = wlr_ext_workspace_handle_v1_create(group);
 
                 std::string name =
                     output->to_string() + ":workspace-" +
                     std::to_string(i * ws_dim.width + j);
-                wlr_workspace_handle_v1_set_name(workspaces[i][j], name.c_str());
+                wlr_ext_workspace_handle_v1_set_name(workspaces[i][j], name.c_str());
 
                 wl_array coordinates;
                 wl_array_init(&coordinates);
                 *(int32_t*)wl_array_add(&coordinates, sizeof(int32_t)) = i;
                 *(int32_t*)wl_array_add(&coordinates, sizeof(int32_t)) = j;
-                wlr_workspace_handle_v1_set_coordinates(
+                wlr_ext_workspace_handle_v1_set_coordinates(
                     workspaces[i][j], &coordinates);
                 wl_array_release(&coordinates);
+
+                on_ws_remove[i][j].set_callback([&] (void*)
+                {
+                    LOGD("Application requested removal of workspace (",
+                        i, ", ", j, ")");
+                });
+
+                on_ws_remove[i][j].connect(&workspaces[i][j]->events.remove_request);
             }
         }
 
         /* Initially, workspace 0,0 is active */
-        wlr_workspace_handle_v1_set_active(workspaces[0][0], true);
+        wlr_ext_workspace_handle_v1_set_active(workspaces[0][0], true);
         output->connect_signal("workspace-changed", &on_current_workspace_changed);
 
         /* Listen for client requests */
@@ -81,7 +100,7 @@ class wlr_workspaces_intergration : public plugin_interface_t
                 for (int j = 0; j < ws_dim.width; j++)
                 {
                     if (workspaces[i][j]->current &
-                        WLR_WORKSPACE_HANDLE_V1_STATE_ACTIVE)
+                        WLR_EXT_WORKSPACE_HANDLE_V1_STATE_ACTIVE)
                     {
                         // workspaces in core are [column, row]
                         active_workspace = {j, i};
@@ -97,21 +116,21 @@ class wlr_workspaces_intergration : public plugin_interface_t
     signal_connection_t on_current_workspace_changed = [&] (signal_data_t *data)
     {
         auto ev = static_cast<wf::workspace_changed_signal*>(data);
-        wlr_workspace_handle_v1_set_active(
+        wlr_ext_workspace_handle_v1_set_active(
             workspaces[ev->old_viewport.y][ev->old_viewport.x], false);
-        wlr_workspace_handle_v1_set_active(
+        wlr_ext_workspace_handle_v1_set_active(
             workspaces[ev->new_viewport.y][ev->new_viewport.x], true);
     };
 
     void fini() override
     {
-        auto manager = wf::get_core().get_data<wlr_workspaces_manager>();
+        auto manager = wf::get_core().get_data<wlr_ext_workspaces_manager>();
         --manager->refcount;
 
         if (manager->refcount <= 0)
         {
             /** Make sure to clean up global data on shutdown */
-            wf::get_core().erase_data<wlr_workspaces_manager>();
+            wf::get_core().erase_data<wlr_ext_workspaces_manager>();
         }
     }
 
@@ -124,4 +143,4 @@ class wlr_workspaces_intergration : public plugin_interface_t
 };
 }
 
-DECLARE_WAYFIRE_PLUGIN(wf::wlr_workspaces_intergration);
+DECLARE_WAYFIRE_PLUGIN(wf::wlr_ext_workspaces_intergration);
