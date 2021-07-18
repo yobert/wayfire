a40f28b5b5e701bfc2fc3db0f9a8a76780cdf19f
Ilia Bozhinov <ammen99@gmail.com>
2021-07-18 15:43:29 +0200

    view: add wrapper for wlr surface locking

---
+++ src/view/surface-lock.cpp
@@ -0,0 +1,94 @@
+#include "surface-lock.hpp"
+#include <wayfire/nonstd/wlroots-full.hpp>
+#include <assert.h>
+
+#include <iostream>
+#define _ << " " <<
+#define debug(x) #x << " = " << x
+
+wf::wlr_surface_manager_t::wlr_surface_manager_t(wlr_surface* surface)
+{
+    this->surface = surface;
+}
+
+uint64_t wf::wlr_surface_manager_t::lock()
+{
+    if (current_checkpoint.has_value())
+    {
+        // Already had a lock, check whether to release next checkpoint
+        // In any case, we just keep the current checkpoint
+        if (this->next_checkpoint)
+        {
+            wlr_surface_unlock_cached(this->surface, next_checkpoint.value());
+            next_checkpoint = {};
+        }
+    } else
+    {
+        // No locks so far, just lock current state.
+        current_checkpoint = wlr_surface_lock_pending(surface);
+    }
+
+    return ++last_id;
+}
+
+void wf::wlr_surface_manager_t::unlock(uint64_t id)
+{
+    if (id != last_id)
+    {
+        return;
+    }
+
+    assert(current_checkpoint.has_value());
+    auto to_release = current_checkpoint.value();
+    current_checkpoint = next_checkpoint;
+    wlr_surface_unlock_cached(surface, to_release);
+    next_checkpoint = {};
+}
+
+void wf::wlr_surface_manager_t::unlock_all(uint64_t id)
+{
+    if (id != last_id)
+    {
+        return;
+    }
+
+    std::vector<uint32_t> to_release;
+
+    if (next_checkpoint.has_value())
+    {
+        to_release.push_back(next_checkpoint.value());
+    }
+
+    if (current_checkpoint.has_value())
+    {
+        to_release.push_back(current_checkpoint.value());
+    }
+
+    current_checkpoint = {};
+    next_checkpoint = {};
+
+    for (auto& pts : to_release)
+    {
+        wlr_surface_unlock_cached(surface, pts);
+    }
+}
+
+bool wf::wlr_surface_manager_t::is_locked() const
+{
+    return current_checkpoint.has_value();
+}
+
+void wf::wlr_surface_manager_t::checkpoint(uint64_t id)
+{
+    if (id != last_id)
+    {
+        return;
+    }
+
+    auto old_checkpoint = next_checkpoint;
+    next_checkpoint = wlr_surface_lock_pending(surface);
+    if (old_checkpoint.has_value())
+    {
+        wlr_surface_unlock_cached(surface, old_checkpoint.value());
+    }
+}
---
+++ test/surface/surface-test.cpp
@@ -0,0 +1,115 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest/doctest.h>
+
+#include "../stub.h"
+#include <wayfire/nonstd/wlroots-full.hpp>
+#include "../../src/view/surface-lock.hpp"
+
+
+std::map<wlr_surface*, int> nr_locks;
+static uint32_t mock_lock_pending(wlr_surface *surface)
+{
+    ++nr_locks[surface];
+    return nr_locks[surface];
+}
+
+std::map<wlr_surface*, std::set<uint32_t>> unlocked;
+static void mock_unlock_pending(wlr_surface *surface, uint32_t id)
+{
+    unlocked[surface].insert(id);
+}
+
+TEST_CASE("wlr_surface_manager_t")
+{
+    wlr_surface surface;
+    nr_locks.clear();
+    unlocked.clear();
+
+    STUB(wlr_surface_lock_pending, mock_lock_pending);
+    STUB(wlr_surface_unlock_cached, mock_unlock_pending);
+
+    wf::wlr_surface_manager_t lockmgr(&surface);
+
+    SUBCASE("No locks - commit passes through")
+    {
+        REQUIRE(nr_locks[&surface] == 0);
+        REQUIRE(lockmgr.is_locked() == false);
+    }
+
+    SUBCASE("Create a checkpoint")
+    {
+        int lock_id = lockmgr.lock();
+        REQUIRE(nr_locks[&surface] == 1);
+        REQUIRE(lockmgr.is_locked() == true);
+
+        SUBCASE("Hop checkpoints")
+        {
+            lockmgr.checkpoint(lock_id);
+            REQUIRE(nr_locks[&surface] == 2);
+            REQUIRE(unlocked[&surface].empty());
+
+            lockmgr.checkpoint(lock_id);
+            REQUIRE(nr_locks[&surface] == 3);
+            REQUIRE(unlocked[&surface] == std::set<uint32_t>{2});
+
+            lockmgr.checkpoint(lock_id);
+            REQUIRE(nr_locks[&surface] == 4);
+            REQUIRE(unlocked[&surface] == std::set<uint32_t>{2, 3});
+            REQUIRE(lockmgr.is_locked() == true);
+        }
+
+        SUBCASE("Reach checkpoint")
+        {
+            lockmgr.checkpoint(lock_id);
+            REQUIRE(nr_locks[&surface] == 2);
+            REQUIRE(unlocked[&surface].empty());
+
+            SUBCASE("Unlock commits up to checkpoint")
+            {
+                lockmgr.unlock(lock_id);
+                REQUIRE(unlocked[&surface] == std::set<uint32_t>{1});
+
+                SUBCASE("Unlock all commits")
+                {
+                    lockmgr.unlock_all(lock_id);
+                    REQUIRE(unlocked[&surface] == std::set<uint32_t>{1, 2});
+                }
+
+                SUBCASE("Transfer lock")
+                {
+                    lockmgr.lock(); // new lock holds state 2
+                    lockmgr.unlock_all(lock_id);
+                    REQUIRE(unlocked[&surface] == std::set<uint32_t>{1});
+                }
+            }
+
+            SUBCASE("Stay at first checkpoint")
+            {
+                lockmgr.lock(); // First lock not released -> remove checkpoint
+                REQUIRE(unlocked[&surface] == std::set<uint32_t>{2});
+
+                // Unlock the first lock.
+                // Only the second checkpoint should be removed!
+                lockmgr.unlock(lock_id);
+                lockmgr.unlock_all(lock_id);
+                REQUIRE(unlocked[&surface] == std::set<uint32_t>{2});
+                REQUIRE(lockmgr.is_locked() == true);
+            }
+        }
+
+        SUBCASE("Stay at first checkpoint")
+        {
+            lockmgr.lock();
+            REQUIRE(unlocked[&surface].empty());
+            REQUIRE(lockmgr.is_locked() == true);
+
+            // Unlock the first lock.
+            // Only the second checkpoint should be removed!
+            lockmgr.unlock(lock_id);
+            lockmgr.unlock_all(lock_id);
+            REQUIRE(unlocked[&surface].empty());
+            REQUIRE(lockmgr.is_locked() == true);
+        }
+    }
+}
