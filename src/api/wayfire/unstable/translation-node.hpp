871fb7ebcaae389897af724c2a139447dd1a62b5
Ilia Bozhinov <ammen99@gmail.com>
2023-06-04 15:31:23 +0200

    translation-node: add render instance class to the header file

--- src/api/wayfire/unstable/translation-node.hpp
+++ src/api/wayfire/unstable/translation-node.hpp
@@ -38,5 +38,26 @@ class translation_node_t : public wf::scene::floating_inner_node_t
   protected:
     wf::point_t offset = {0, 0};
 };
+
+class translation_node_instance_t : public render_instance_t
+{
+  protected:
+    std::vector<render_instance_uptr> children;
+    damage_callback push_damage;
+    translation_node_t *self;
+    wf::signal::connection_t<wf::scene::node_damage_signal> on_node_damage;
+
+  public:
+    translation_node_instance_t(translation_node_t *self,
+        damage_callback push_damage, wf::output_t *shown_on);
+
+    // Implementation of render_instance_t
+    void schedule_instructions(std::vector<wf::scene::render_instruction_t>& instructions,
+        const wf::render_target_t& target, wf::region_t& damage) override;
+    void render(const wf::render_target_t& target, const wf::region_t& region) override;
+    void presentation_feedback(wf::output_t *output) override;
+    wf::scene::direct_scanout try_scanout(wf::output_t *output) override;
+    void compute_visibility(wf::output_t *output, wf::region_t& visible) override;
+};
 }
 }
--- src/view/translation-node.cpp
+++ src/view/translation-node.cpp
@@ -23,93 +23,6 @@ std::string wf::scene::translation_node_t::stringify() const
     return "translation by " + x + "," + y + " " + stringify_flags();
 }
 
-namespace wf
-{
-namespace scene
-{
-class translation_node_instance_t : public wf::scene::render_instance_t
-{
-    std::vector<wf::scene::render_instance_uptr> children;
-    wf::scene::damage_callback push_damage;
-    wf::scene::translation_node_t *self;
-
-  public:
-    translation_node_instance_t(wf::scene::translation_node_t *self,
-        wf::scene::damage_callback push_damage, wf::output_t *shown_on)
-    {
-        this->self = self;
-        this->push_damage = push_damage;
-
-        self->connect(&on_node_damage);
-
-        auto push_damage_child = [=] (wf::region_t child_damage)
-        {
-            child_damage += self->get_offset();
-            push_damage(child_damage);
-        };
-
-        for (auto& ch : self->get_children())
-        {
-            if (ch->is_enabled())
-            {
-                ch->gen_render_instances(children, push_damage_child, shown_on);
-            }
-        }
-    }
-
-    wf::signal::connection_t<wf::scene::node_damage_signal> on_node_damage =
-        [=] (wf::scene::node_damage_signal *data)
-    {
-        push_damage(data->region);
-    };
-
-    void schedule_instructions(std::vector<wf::scene::render_instruction_t>& instructions,
-        const wf::render_target_t& target, wf::region_t& damage) override
-    {
-        wf::point_t offset = self->get_offset();
-        damage += -offset;
-
-        auto our_target = target.translated(-offset);
-        for (auto& ch : this->children)
-        {
-            ch->schedule_instructions(instructions, our_target, damage);
-        }
-
-        damage += offset;
-    }
-
-    void render(const wf::render_target_t& target,
-        const wf::region_t& region) override
-    {
-        wf::dassert(false, "Rendering a translation node?");
-    }
-
-    void presentation_feedback(wf::output_t *output) override
-    {
-        for (auto& ch : this->children)
-        {
-            ch->presentation_feedback(output);
-        }
-    }
-
-    wf::scene::direct_scanout try_scanout(wf::output_t *output) override
-    {
-        if (self->get_offset() != wf::point_t{0, 0})
-        {
-            return wf::scene::direct_scanout::OCCLUSION;
-        }
-
-        return try_scanout_from_list(this->children, output);
-    }
-
-    void compute_visibility(wf::output_t *output, wf::region_t& visible) override
-    {
-        compute_visibility_from_list(children, output, visible, self->get_offset());
-    }
-};
-}
-}
-
 void wf::scene::translation_node_t::gen_render_instances(
     std::vector<scene::render_instance_uptr>& instances,
     scene::damage_callback damage, wf::output_t *output)
@@ -130,4 +43,81 @@ wf::point_t wf::scene::translation_node_t::get_offset() const
 void wf::scene::translation_node_t::set_offset(wf::point_t offset)
 {
     this->offset = offset;
+}
+
+// ----------------------------------------- Render instance -------------------------------------------------
+wf::scene::translation_node_instance_t::translation_node_instance_t(
+    translation_node_t *self, damage_callback push_damage, wf::output_t *shown_on)
+{
+    this->self = self;
+    this->push_damage = push_damage;
+
+    on_node_damage = [=] (wf::scene::node_damage_signal *data)
+    {
+        push_damage(data->region);
+    };
+    self->connect(&on_node_damage);
+
+    auto push_damage_child = [=] (wf::region_t child_damage)
+    {
+        child_damage += self->get_offset();
+        push_damage(child_damage);
+    };
+
+    for (auto& ch : self->get_children())
+    {
+        if (ch->is_enabled())
+        {
+            ch->gen_render_instances(children, push_damage_child, shown_on);
+        }
+    }
+}
+
+void wf::scene::translation_node_instance_t::schedule_instructions(
+    std::vector<wf::scene::render_instruction_t>& instructions,
+    const wf::render_target_t& target, wf::region_t& damage)
+{
+    wf::region_t our_damage = damage & self->get_bounding_box();
+    if (!our_damage.empty())
+    {
+        wf::point_t offset = self->get_offset();
+        damage += -offset;
+        auto our_target = target.translated(-offset);
+
+        for (auto& ch : this->children)
+        {
+            ch->schedule_instructions(instructions, our_target, damage);
+        }
+
+        damage += offset;
+    }
+}
+
+void wf::scene::translation_node_instance_t::render(
+    const wf::render_target_t& target, const wf::region_t& region)
+{
+    wf::dassert(false, "Rendering a translation node?");
+}
+
+void wf::scene::translation_node_instance_t::presentation_feedback(wf::output_t *output)
+{
+    for (auto& ch : this->children)
+    {
+        ch->presentation_feedback(output);
+    }
+}
+
+wf::scene::direct_scanout wf::scene::translation_node_instance_t::try_scanout(wf::output_t *output)
+{
+    if (self->get_offset() != wf::point_t{0, 0})
+    {
+        return wf::scene::direct_scanout::OCCLUSION;
+    }
+
+    return try_scanout_from_list(this->children, output);
+}
+
+void wf::scene::translation_node_instance_t::compute_visibility(wf::output_t *output, wf::region_t& visible)
+{
+    compute_visibility_from_list(children, output, visible, self->get_offset());
 }
